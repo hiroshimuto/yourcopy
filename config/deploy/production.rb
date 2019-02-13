afdc80b17898acef35c6684a00b0fcdecfcd607a
hiroshimuto <hiroshi.github@gmail.com>
2019-02-13 17:45:49 +0900

    edit unicorn.rb and deploy.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -6,6 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+
+server '13.112.214.162', user: 'ec2-user', roles: %w{app db web}
 
 
 
