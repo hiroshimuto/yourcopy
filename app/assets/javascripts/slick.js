e812255b85c7116fee43cd72fc08179f0d7d47db
hiroshimuto <hiroshi.github@gmail.com>
2019-02-15 18:35:24 +0900

    set swiper

---
+++ app/assets/javascripts/slick.js
@@ -0,0 +1,10 @@
+$(function(){
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: false,
+    slidesPerView: 2,
+    spaceBetween: 10,
+    centeredSlides : true,
+    pagination: '.swiper-pagination',
+  })
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root 'home#index'
+
+  devise_scope :user do
+    get '/', to: 'devise/registrations#new'
+  end
+
+  get '/home' => 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
