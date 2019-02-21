110e76b152e69c89c41222f64649d0d75f95e13b
hiroshimuto <hiroshi.github@gmail.com>
2019-02-21 14:55:53 +0900

    create tweet function

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -9,10 +9,11 @@ class CopiesController < ApplicationController
     @copy = Copy.new(copy_params)
     binding.pry
     if @copy.save
+
        @twitter.update("#{@copy.text}\r")
       redirect_to root_path
     else
-      render 'new'
+      render plain: "しっぱい〜〜〜"
     end
   end
 
@@ -24,10 +25,10 @@ class CopiesController < ApplicationController
 
   def set_twitter_client
     @twitter = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "TWITTER_API_KEY"
-      config.consumer_secret     = "TWITTER_API_SECRET"
-      config.access_token        = "TWITTER_ACCESS_TOKEN"
-      config.access_token_secret = "TWITTER_ACCESS_SECRET"
+      config.consumer_key        = ENV['TWITTER_API_KEY']
+      config.consumer_secret     = ENV['TWITTER_API_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
     end
   end
 end
