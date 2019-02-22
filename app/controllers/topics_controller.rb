8c2618b9d9a5e9ae0c40c52ff7c442fab3305b5f
hiroshimuto <hiroshi.github@gmail.com>
2019-02-22 19:23:30 +0900

    create controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,14 @@
 class TopicsController < ApplicationController
+  before_action :topic_info, only:[:show]
   def show
-
+    @copies = Copy.where(topic_id: @topic).includes(:user)
   end
+
+  private
+
+  def topic_info
+    @theme = Theme.find(params[:id])
+    @topic = @theme.topics.find(params[:id])
+  end
+
 end
